5dc37fbc8545e5c17e0d8cf8461840b19f009dd7
Maulika <Maulika@DESKTOP-I3OP9II>
2018-12-10 16:42:41 -0500

    final exam completed

---
+++ F2018Travel.Tests/Controllers/RegionsControllerTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// refs
+using F2018Travel.Controllers;
+using F2018Travel.Models;
+using System.Web.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace F2018Travel.Tests.Controllers
+{
+    [TestClass]
+    public class RegionsControllerTest
+    {
+        RegionsController controller;
+        Mock<IMockRegions> mock;
+        List<Region> regions;
+        Region region;
+
+        [TestInitialize]
+        public void TestInitalize()
+        {
+            mock = new Mock<IMockRegions>();
+            regions = new List<Region>
+            {
+                new Region { RegionId = 78, Region1 = "Oceania" },
+                new Region { RegionId = 349, Region1 = "Eurasia" },
+                new Region { RegionId = 205, Region1 = "Eastasia" },
+            };
+
+            region = new Region
+            {
+                RegionId = 489, Region1 = "Disputed"
+            };
+
+            mock.Setup(m => m.Regions).Returns(regions.AsQueryable());
+            controller = new RegionsController(mock.Object);
+        }
+
+        [TestMethod]
+        //loads the index view
+        public void IndexLoadsView()
+        {
+            //arrange
+
+            //act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [TestMethod]
+        //Returns a list of all Regions
+        public void IndexValidLoadsRegions()
+        {
+            //act
+            var result = (List<Region>)((ViewResult)controller.Index()).Model;
+
+            // assert
+            CollectionAssert.AreEqual(regions, result);
+        }
+
+        [TestMethod]
+        //Returns the selected Region
+        public void DetailsValidId()
+        {
+            // act - cast the model as an Album object
+            Region actual = (Region)((ViewResult)controller.Details(78)).Model;
+
+            // assert - is this the first mock album in our array?
+            Assert.AreEqual(regions[0], actual);
+        }
+
+        [TestMethod]
+        //Loads the Error view
+        public void DetailsInvalidId()
+        {
+            // act
+            var result = (ViewResult)controller.Details(3456);
+
+            // assert
+            Assert.AreEqual("Error", result.ViewName);
+        }
+
+        [TestMethod]
+        //Loads the Error view
+        public void DetailsNoId()
+        {
+            // act
+            var result = (ViewResult)controller.Details(null);
+
+            // assert
+            Assert.AreEqual("Error", result.ViewName);
+        }
+    }
+}
---
+++ F2018Travel/Controllers/api/RegionsController.cs
@@ -0,0 +1,104 @@
+﻿using F2018Travel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace F2018Travel.Controllers.api
+{
+    public class RegionsController : ApiController
+    {
+        private ExamModel db = new ExamModel();
+
+        // GET: api/Regions
+        public IQueryable<Region> GetDestinations()
+        {
+            return db.Regions;
+        }
+        // GET: api/Regions/5
+        [ResponseType(typeof(Destination))]
+        public IHttpActionResult GetRegion(int id)
+        {
+            Region region = db.Regions.Find(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+        }
+        // PUT: api/Regions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutRegion(int id, Region region)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != region.RegionId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(region).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Regions
+        [ResponseType(typeof(Destination))]
+        public IHttpActionResult PostRegion(Region region)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Regions.Add(region);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = region.RegionId }, region);
+        }
+
+        // DELETE: api/Regions/5
+        [ResponseType(typeof(Destination))]
+        public IHttpActionResult DeleteRegion(int id)
+        {
+            Region region = db.Regions.Find(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            db.Regions.Remove(region);
+            db.SaveChanges();
+
+            return Ok(region);
+        }
+        private bool RegionExists(int id)
+        {
+            return db.Regions.Count(r => r.RegionId == id) > 0;
+        }
+    }
+}
